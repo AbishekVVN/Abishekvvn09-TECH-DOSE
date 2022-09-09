710918ba2ab0f8b089e57a102984a6120bda2ca1
AbishekVVN <110518545+AbishekVVN@users.noreply.github.com>
2022-09-09 12:29:22 +0530

    Update pascalstriangle(as2darray).c

--- SORTING ALGORITHMS/ASSIGNEMENTS(BEGINNER)/pascalstriangle(as2darray).c
+++ SORTING ALGORITHMS/ASSIGNEMENTS(BEGINNER)/pascalstriangle(as2darray).c
@@ -1,24 +1,27 @@
-#include <stdio.h> 
-#include <stdlib.h> 
- 
-int main(void){ 
-    int n, k ;  
-    printf("Enter n:\n"); 
-    scanf("%d",&n); 
-    printf("Enter k:\n"); 
-    scanf("%d",&k); 
-    int pascal[n+1][n+1]; 
-    int i,j ; 
-    for(i=0;i<n+1;++i){ 
-        pascal[i][0] = 1 ;  
-        pascal[i][i] = 1 ;  
-    } 
- 
-    for(i=2;i<n+1;++i){ 
-        for(j=1;j<i;++j){ 
-            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]; 
-        } 
-    } 
-    printf("%d\n", pascal[n][k]); 
-    return 0 ;  
-} 
+int PascalsTriangle(int n) {
+    int arr[100][100];
+    int i, j;
+    for (i = 0; i < n; ++i) {
+        // printing spaces
+        for (j = 0; j < n - 1 - i; ++j) {
+            printf(" ");
+        }
+        // calculating then printing the data of ith
+        // row of Pascal's Trianle
+        for (j = 0; j <= i; ++j) {
+            if (j == 0 || j == i)
+                arr[i][j] = 1;
+            else
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter Number of Rows in Pascal's Trianle : ");
+    scanf("%d", &n);
+    PascalsTriangle(n);
+    return 0;
+}
