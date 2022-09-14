82565cf3f654be8f60376ffffaac5e1004737c9d
AbishekVVN <110518545+AbishekVVN@users.noreply.github.com>
2022-09-14 13:58:46 +0530

    Update concatenation.java

--- SORTING ALGORITHMS/ASSIGNEMENTS(BEGINNER)/concatenation.java
+++ SORTING ALGORITHMS/ASSIGNEMENTS(BEGINNER)/concatenation.java
@@ -1,16 +1,14 @@
-public class Concat {
-
-    public static void main(String[] args) {
-        int[] array1 = {1, 2, 3};
-        int[] array2 = {4, 5, 6};
-
-        int aLen = array1.length;
-        int bLen = array2.length;
-        int[] result = new int[aLen + bLen];
-
-        System.arraycopy(array1, 0, result, 0, aLen);
-        System.arraycopy(array2, 0, result, aLen, bLen);
-
-        System.out.println(Arrays.toString(result));
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        int ans[] = new int[nums.length*2];
+        
+        for(int i = 0 ; i < nums.length ; ++i){
+            ans[i] = nums[i];
+            ans[i+nums.length] = ans[i];
+        }
+        
+        return ans;
+        
     }
 }
