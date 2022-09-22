36f5ff0c9f689ce5fad3b42b823fa45fbb879831
AbishekVVN <110518545+AbishekVVN@users.noreply.github.com>
2022-09-22 10:33:38 +0530

    Create Largest_ sum_contiguous_subarray.cpp

---
+++ ARRAYS/Assignment(beginners)/Largest_ sum_contiguous_subarray.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int maximumSubarraySum(vector <int>arr){
+  int n=arr.size();
+  int maxSum=INT_MIN;
+  
+  for(int i=0;i<=n;i++){
+    int currSum=0;
+    for(int j=i;j<=n;j++){
+      currSum+=arr[j];
+      if(currSum>maxSum){
+        maxSum=currSum;
+      }
+    }
+  }
+  return maxSum;
+}
+int main(){
+  vector<int>a={1,,3,8,-2,6,-8,5};
+  cout<<maximumSubarraySum(a)<<endl;
+  return 0;
+}
