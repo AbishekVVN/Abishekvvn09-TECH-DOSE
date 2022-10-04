5448d2ed87990bdd61fb948fd03bf169e232c819
AbishekVVN <110518545+AbishekVVN@users.noreply.github.com>
2022-10-04 14:19:30 +0530

    Create Sum2.java

---
+++ ARRAYS/Assignment(beginners)/Sum2.java
@@ -0,0 +1,15 @@
+class Solution {
+public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+	var pairCountBySum = new HashMap<Integer, Integer>();
+	
+	Arrays.stream(nums1)
+		  .forEach(num1 -> Arrays.stream(nums2)
+							     .forEach(num2 -> pairCountBySum.compute(num1 + num2, (k, sumCount) -> sumCount == null ? 1 : ++sumCount)));
+		  
+	return Arrays.stream(nums3)
+				 .map(num3 -> Arrays.stream(nums4)
+								    .map(num4 -> pairCountBySum.getOrDefault(-(num3 + num4), 0))
+									.sum())
+				 .sum();
+}
+}
