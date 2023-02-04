9df810bc36df44a83c732b7f43f4a42c45952ca9
Abishek Venu Venkat N <110518545+AbishekVVN@users.noreply.github.com>
2023-02-04 14:10:41 +0530

    Create implement_trie.cpp

---
+++ Trie/implement_trie.cpp
@@ -0,0 +1,42 @@
+const int mxN = 26;
+
+class Trie {
+private:
+    Trie *children[mxN];
+    bool is_end;
+
+public:
+    Trie() {
+        for(int i=0; i<mxN; ++i) children[i] = nullptr;
+        is_end = false;
+    }
+    
+    void insert(string word) {
+        Trie *curr = this;
+        for(int i=0; i<word.size(); ++i) {
+            int idx = word[i] - 'a';
+            if(curr->children[idx] == nullptr) {
+                curr->children[idx] = new Trie();
+            }
+            curr = curr->children[idx];
+        }
+        curr->is_end = true;
+    }
+    
+    bool search(string word, bool is_prefix=false) {
+        Trie *curr = this;
+        for(int i=0; i<word.size(); ++i) {
+            int idx = word[i] - 'a';
+            if(curr->children[idx] == nullptr) return false;
+
+            curr = curr->children[idx];
+        }
+
+        if(is_prefix) return true;
+        return curr->is_end;
+    }
+    
+    bool startsWith(string prefix) {
+        return search(prefix, true);
+    }
+};
